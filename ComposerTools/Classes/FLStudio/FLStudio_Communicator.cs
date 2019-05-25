fffb012adbfa8f2700fdc2a60ea441c37546fead
Kevin Müller <kevin.mueller1@gmx.ch>
2019-05-25 23:59:39 +0200

    cmt

--- ComposerTools/Classes/FLStudio/FLStudio_Communicator.cs
+++ ComposerTools/Classes/FLStudio/FLStudio_Communicator.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows;
 using System.Windows.Forms;
@@ -10,10 +9,7 @@ namespace ComposerTools.Classes.FLStudio
 {
     class FLStudio_Communicator
     {
-        [DllImport("User32.dll")]
-        public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
-        [DllImport("user32.dll")]
-        static extern bool SetForegroundWindow(IntPtr hWnd);
+
 
         private static FLStudio_Communicator instance;
 
@@ -37,51 +33,21 @@ namespace ComposerTools.Classes.FLStudio
         //These Methods are void because the midi gets copied to the clipboard
         public void getClipboardFromFL()
         {
-            System.Windows.Clipboard.Clear();
-            System.Windows.Forms.Clipboard.Clear();
-
-            UIItemCollection coll = window.Items;
-            foreach (UIItem item in coll)
-            {
-                if (item.Name.Contains("Piano roll"))
-                {
-                    if (((TestStack.White.UIItems.Panel)item).Items.Count == 0)
-                    {
-                        itemToClick = (TestStack.White.UIItems.Panel)item;
-                        break;
-                    }
-                }
-            }
-
-            SetForegroundWindow(windowHandle);
-            window.Focus();
-            var mouse = window.Mouse;
-            Point p = itemToClick.Location;
-            p.Offset(11, 12);
-
-            mouse.Click(p);
-            SendKeys.SendWait("f");
-            SendKeys.SendWait("c");
+            FL_Interaction.openPianoRollContextMenu(windowHandle, window);
+            FL_Interaction.openPianoRollContextMenuFile(windowHandle, window);
+            SendKeys.SendWait("c"); //->Copy Midi To Clipboard
             Thread.Sleep(100); //TODO: This delay?? (It's needed because otherwise the midi is not copied to the clipboard)
             SendKeys.Flush();
         }
 
         public void sendClipboardToFLStudio()
         {
-            bool focus = SetForegroundWindow(windowHandle);
-            if (focus)
-            {
-                var mouse = window.Mouse;
-                Point p = itemToClick.Location;
-                p.Offset(11, 12);
-                mouse.Click(p);
-                SendKeys.SendWait("f");
-                SendKeys.SendWait("p");
-                SendKeys.Flush();
-            } else
-            {
-                Logger.Log("Unable to get Focus when sending clipboard back to fl");
-            }
+            TestStack.White.UIItemEvents.UIItemClickEvent t = new TestStack.White.UIItemEvents.UIItemClickEvent(itemToClick);
+            FL_Interaction.openPianoRollContextMenu(windowHandle, window);
+            SendKeys.SendWait("f"); //->File
+            SendKeys.SendWait("p"); //->Paste Midi From Clipboard
+            SendKeys.Flush();
+
         }
 
         public void openFlStudio()
